828aebd6d667fa441ff1d86dc41bce4bef439809
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-01-23 06:24:10 -0500

    Complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+// To run tests, use command in terminal below:
+// npm test getTheTitles.spec.js
 
+// use the .map method on existing array, use it to create a new array that contains the titles
+
+// Resources: https://www.youtube.com/watch?v=HB1ZC7czKRs
+
+// https://discord.com/channels/505093832157691914/505093832157691916/1054201913236791316
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+
+
+  const getTheTitles = function(books) {
+    return books.map(book => book.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
